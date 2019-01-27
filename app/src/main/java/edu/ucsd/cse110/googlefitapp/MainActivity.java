108475d2910919661aa7f79a1ac87f6157cc4902
Sander Valstar <sander@email.com>
2019-01-26 22:48:46 -0800

    initial commit

---
+++ app/src/main/java/edu/ucsd/cse110/googlefitapp/MainActivity.java
@@ -0,0 +1,46 @@
+package edu.ucsd.cse110.googlefitapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import edu.ucsd.cse110.googlefitapp.fitness.FitnessService;
+import edu.ucsd.cse110.googlefitapp.fitness.FitnessServiceFactory;
+import edu.ucsd.cse110.googlefitapp.fitness.GoogleFitAdapter;
+
+public class MainActivity extends AppCompatActivity {
+    private String fitnessServiceKey = "GOOGLE_FIT";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnGoToSteps = findViewById(R.id.buttonGoToSteps);
+        btnGoToSteps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                launchStepCountActivity();
+            }
+        });
+
+        FitnessServiceFactory.put(fitnessServiceKey, new FitnessServiceFactory.BluePrint() {
+            @Override
+            public FitnessService create(StepCountActivity stepCountActivity) {
+                return new GoogleFitAdapter(stepCountActivity);
+            }
+        });
+    }
+
+    public void launchStepCountActivity() {
+        Intent intent = new Intent(this, StepCountActivity.class);
+        intent.putExtra(StepCountActivity.FITNESS_SERVICE_KEY, fitnessServiceKey);
+        startActivity(intent);
+    }
+
+    public void setFitnessServiceKey(String fitnessServiceKey) {
+        this.fitnessServiceKey = fitnessServiceKey;
+    }
+}
