1fe4f65e0a0af1f4eb4a8bb33e37c2d57f8cb935
Max <maximilian.paulsen@gmail.com>
2020-02-07 12:13:23 -0800

    Added some navigation

--- app/src/main/java/edu/ucsd/cse110/walkstatic/RunActivity.java
+++ app/src/main/java/edu/ucsd/cse110/walkstatic/RunActivity.java
@@ -30,7 +30,7 @@ import edu.ucsd.cse110.walkstatic.fitness.FitnessService;
 import edu.ucsd.cse110.walkstatic.fitness.FitnessServiceFactory;
 import edu.ucsd.cse110.walkstatic.fitness.GoogleFitAdapter;
 
-public class MainActivity extends AppCompatActivity {
+public class RunActivity extends AppCompatActivity {
     private static String fitnessServiceKey = "GOOGLE_FIT";
     public static void setFitnessServiceKey(String newKey) {
         fitnessServiceKey = newKey;
@@ -38,17 +38,16 @@ public class MainActivity extends AppCompatActivity {
 
     private StepTracker stepTracker;
     private FitnessService fitnessService;
-    private ActionBarDrawerToggle toggle;
 
     private static final String TAG = "StepCountActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
-        //initStepCount();
-        setupNavBar();
+        setContentView(R.layout.activity_run);
+
+        //initStepCount(); TODO uncomment!
+        setupDrawerButton();
     }
 
     @Override
@@ -100,39 +99,14 @@ public class MainActivity extends AppCompatActivity {
         textSteps.setText(Long.toString(steps));
     }
 
-    private void setupNavBar(){
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        ActionBar actionBar = getActionBar();
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setHomeButtonEnabled(true);
-        toggle = new ActionBarDrawerToggle(this, drawer,  R.string.open_drawer, R.string.close_drawer);
-        drawer.addDrawerListener(toggle);
-        toggle.setDrawerIndicatorEnabled(true);
-        toggle.syncState();
-        //this.populateNavList();
-
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        NavigationUI.setupWithNavController(navigationView, navController);
-    }
-
-//    private void populateNavList(){
-//        ListView listView = findViewById(R.id.list_drawer);
-//        List<String> listItems = Arrays.asList(new String[]{"Current Run", "My Runs"});
-//        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-//                R.layout.hamburger_textview,
-//                listItems);
-//        listView.setAdapter(adapter);
-//        adapter.notifyDataSetChanged();
-//    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
-        if (toggle.onOptionsItemSelected(item))
-        {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+    private void setupDrawerButton(){
+            DrawerLayout drawer = findViewById(R.id.drawer_layout);
+            ActionBar actionBar = getActionBar();
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setHomeButtonEnabled(true);
+            ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer,  R.string.open_drawer, R.string.close_drawer);
+            drawer.addDrawerListener(toggle);
+            toggle.setDrawerIndicatorEnabled(true);
+            toggle.syncState();
     }
 }
