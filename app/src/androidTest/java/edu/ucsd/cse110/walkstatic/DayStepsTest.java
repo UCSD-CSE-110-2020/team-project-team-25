3dd0c917157e6e79281b7cfb256c196ba3eb8a65
Max <maximilian.paulsen@gmail.com>
2020-02-08 20:10:44 -0800

    Added test for Steps in Previous story (#7)

--- app/src/androidTest/java/edu/ucsd/cse110/walkstatic/DayStepsTest.java
+++ app/src/androidTest/java/edu/ucsd/cse110/walkstatic/DayStepsTest.java
@@ -1,12 +1,6 @@
 package edu.ucsd.cse110.walkstatic;
 
 
-import androidx.test.espresso.ViewInteraction;
-import androidx.test.espresso.matcher.ViewMatchers;
-import androidx.test.filters.LargeTest;
-import androidx.test.rule.ActivityTestRule;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-
 import android.app.Activity;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,12 +8,15 @@ import android.view.ViewParent;
 
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
-import org.hamcrest.Matchers;
 import org.hamcrest.TypeSafeMatcher;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
 import edu.ucsd.cse110.walkstatic.fitness.FitnessListener;
 import edu.ucsd.cse110.walkstatic.fitness.FitnessService;
 import edu.ucsd.cse110.walkstatic.fitness.FitnessServiceFactory;
@@ -28,30 +25,83 @@ import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
-public class MainActivityEspressoTest {
+public class DayStepsTest {
     private static final String TEST_SERVICE = "TEST_SERVICE";
+    public static long stepCount = 1337;
 
     @Rule
     public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
 
     @Test
-    public void mainActivityEspressoTest() {
+    public void dayStepsTest() {
         FitnessServiceFactory.put(TEST_SERVICE, new FitnessServiceFactory.BluePrint() {
             @Override
             public FitnessService create(Activity activity) {
-                return new TestFitnessService(activity);
+                return new DayStepsTest.TestFitnessService(activity);
             }
         });
 
         RunFragment.setFitnessServiceKey(TEST_SERVICE);
 
+        ViewInteraction appCompatImageButton = onView(
+                allOf(withContentDescription("Open navigation drawer"),
+                        childAtPosition(
+                                allOf(withId(R.id.action_bar),
+                                        childAtPosition(
+                                                withId(R.id.action_bar_container),
+                                                0)),
+                                1),
+                        isDisplayed()));
+        appCompatImageButton.perform(click());
 
+        ViewInteraction navigationMenuItemView = onView(
+                allOf(childAtPosition(
+                        allOf(withId(R.id.design_navigation_view),
+                                childAtPosition(
+                                        withId(R.id.nav_view),
+                                        0)),
+                        2),
+                        isDisplayed()));
+        navigationMenuItemView.perform(click());
+
+        ViewInteraction appCompatImageButton2 = onView(
+                allOf(withContentDescription("Navigate up"),
+                        childAtPosition(
+                                allOf(withId(R.id.action_bar),
+                                        childAtPosition(
+                                                withId(R.id.action_bar_container),
+                                                0)),
+                                1),
+                        isDisplayed()));
+        appCompatImageButton2.perform(click());
+
+        stepCount = 10;
+        try {
+            Thread.sleep(2100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        ViewInteraction textView = onView(
+                allOf(withId(R.id.steps_today), withText("10"),
+                        isDisplayed()));
+        textView.check(matches(withText("10")));
+        stepCount = 50;
+        try {
+            Thread.sleep(1100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        ViewInteraction textView2 = onView(
+                allOf(withId(R.id.steps_today), withText("50"),
+                        isDisplayed()));
+        textView2.check(matches(withText("50")));
     }
 
     private static Matcher<View> childAtPosition(
@@ -94,7 +144,7 @@ public class MainActivityEspressoTest {
         public void updateStepCount() {
             System.out.println(TAG + "updateStepCount");
             if(this.listener != null){
-                listener.onNewSteps(1337);
+                listener.onNewSteps(stepCount);
             }
         }
 
