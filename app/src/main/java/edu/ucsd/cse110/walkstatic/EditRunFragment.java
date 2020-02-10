319f1a3c24548220efc7fc72a3df489bf0ce8a29
Max <maximilian.paulsen@gmail.com>
2020-02-09 16:21:58 -0800

    Added Integration test for EditRun

--- app/src/main/java/edu/ucsd/cse110/walkstatic/EditRunFragment.java
+++ app/src/main/java/edu/ucsd/cse110/walkstatic/EditRunFragment.java
@@ -58,13 +58,12 @@ public class EditRunFragment extends Fragment {
         EditText runName = this.getActivity().findViewById(R.id.run_name_text);
         EditText startingPoint = this.getActivity().findViewById(R.id.starting_point_text);
         String runText = runName.getText().toString() + startingPoint.getText();
-        Snackbar.make(this.getView(), "Run Saved", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show();
+
         RunViewModel runViewModel = new ViewModelProvider(this.getActivity()).get(RunViewModel.class);
         runViewModel.setRun(new Run(this.safeGetUUID(), runName.getText().toString()));
         runName.clearFocus();
         startingPoint.clearFocus();
-        Navigation.findNavController(this.getActivity(), this.getId()).navigateUp();
+        Navigation.findNavController(this.getView()).navigateUp();
     }
 
     private int safeGetUUID(){
