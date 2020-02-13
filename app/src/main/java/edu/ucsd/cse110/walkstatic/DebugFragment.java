67d6032045bb7d0f6fa524ec9782f78439f2053f
Pumuckl007 <maximilian.paulsen@gmail.com>
2020-02-12 18:37:27 -0800

    Added debug page

---
+++ app/src/main/java/edu/ucsd/cse110/walkstatic/DebugFragment.java
@@ -0,0 +1,26 @@
+package edu.ucsd.cse110.walkstatic;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class DebugFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_debug, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState){
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+
+
+}
--- app/src/main/java/edu/ucsd/cse110/walkstatic/RunFragment.java
+++ app/src/main/java/edu/ucsd/cse110/walkstatic/RunFragment.java
@@ -8,12 +8,18 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import java.text.DecimalFormat;
 
