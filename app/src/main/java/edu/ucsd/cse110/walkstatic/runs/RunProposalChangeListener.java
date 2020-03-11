0a84b1b4f86df3c2b5bc6301f506e9882490c182
Nathan Nguyen <nathannguyen@nathans-mbp.dynamic.ucsd.edu>
2020-03-10 22:46:43 -0700

    RunProposalChangeListener

---
+++ app/src/main/java/edu/ucsd/cse110/walkstatic/runs/RunProposalChangeListener.java
@@ -0,0 +1,8 @@
+package edu.ucsd.cse110.walkstatic.runs;
+
+public interface RunProposalChangeListener {
+
+    //USE THIS FOR FIREBASEPROPOSEDWATCHER
+    public void onChangedProposal(RunProposal runProposal);
+
+}
--- app/src/main/java/edu/ucsd/cse110/walkstatic/runs/RunProposalListener.java
+++ app/src/main/java/edu/ucsd/cse110/walkstatic/runs/RunProposalListener.java
@@ -5,5 +5,5 @@ import java.util.List;
 import edu.ucsd.cse110.walkstatic.teammate.TeammateResponse;
 
 public interface RunProposalListener {
-    public void onResponsesChanged(RunProposal responseList);
+    public void onResponsesChanged(List<TeammateResponse> responseList);
 }
