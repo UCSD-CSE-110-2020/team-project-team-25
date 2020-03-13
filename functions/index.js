07ad8e963e34958f8a6de940e7c51b14769d056f
Max <maximilian.paulsen@gmail.com>
2020-03-12 22:14:03 -0700

    Added notifications for change in response to scheduled walk closes #170

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,9 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
+const firestore = admin.firestore();
 
-exports.sendChatNotifications = functions.firestore
+exports.sendTeammateRequestNotifications = functions.firestore
    .document('team/requests/{username}/{requestId}')
    .onCreate((snap, context) => {
      // Get an object with the current document value.
@@ -35,4 +36,75 @@ exports.sendChatNotifications = functions.firestore
      }
 
      return "document was null or emtpy";
+   });
+
+var responseToRun = function(document, proposal, responseMsg){
+     var responseString = "";
+       if(document.response === "GOING"){
+            responseString = document.user.name + " can go to your run!";
+       } else if(document.response === "BAD_TIME"){
+            responseString = "The time is a bad time for " + document.user.name;
+       } else if(document.response === "NOT_GOOD"){
+            responseString = "The route is a bad route for " + document.user.name;
+       }
+       var message = {
+         notification: {
+           title: document.user.name + ' ' + responseMsg + ' ' + proposal.run.name,
+           body: responseString
+         },
+         topic: proposal.author.email.replace("@", "")
+       };
+
+       return admin.messaging().send(message)
+         .then((response) => {
+           // Response is a message ID string.
+           console.log('Successfully sent message:', response);
+           return response;
+         })
+         .catch((error) => {
+           console.log('Error sending message:', error);
+           return error;
+         });
+}
+
+exports.sendScheduledRunResponseCreatedNotifications = functions.firestore
+   .document('team/proposedRuns/responses/{responseId}')
+   .onCreate((snap, context) => {
+     // Get an object with the current document value.
+     // If the document does not exist, it has been deleted.
+     const document = snap.exists ? snap.data() : null;
+
+     if (document) {
+
+        return firestore.collection('team').doc('proposals').get().then((proposal) => {
+            if(proposal.exists){
+                return responseToRun(document, proposal.data(), "responded to");
+            }
+            return "failed to read proposal"
+        });
+     }
+
+     return "document was null or emtpy";
+   });
+
+exports.sendScheduledRunResponseUpdatedNotifications = functions.firestore
+   .document('team/proposedRuns/responses/{responseId}')
+   .onUpdate((snap, context) => {
+     // Get an object with the current document value.
+     // If the document does not exist, it has been deleted.
+     const document = snap.exists ? snap.data() : null;
+     console.log("reading changed document at: ", context.params.responseId);
+     var docRef = firestore.collection('team').doc('proposedRuns');
+     docRef = docRef.collection('responses').doc(context.params.responseId);
+     return docRef.get().then((document) => {
+                 if(document.exists){
+                      return firestore.collection('team').doc('proposals').get().then((proposal) => {
+                                 if(proposal.exists){
+                                     return responseToRun(document.data(), proposal.data(), "changed response to");
+                                 }
+                                 return "failed to read proposal"
+                             });
+                 }
+                 return "failed to read changed document"
+             });
    });
